c6186fca16e0d5812e1d7a0b46d1882873dd2e48
egnaf <egnaf@yahoo.com>
2020-01-17 20:20:22 +0300

    Initial project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navigation/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Navigation from "./components/Navigation"
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: "app",
+    components: {
+      Navigation
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+  }
+  a {
+    color: royalblue;
+  }
 </style>
---
+++ src/layouts/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Login
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "login",
+    components: {
+    }
+  }
+</script>
